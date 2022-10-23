4601097b25e80c78e22d7d0beadbdaba733c4718
LarbiAmineGIT <larbiamine22@yahoo.fr>
2022-10-23 17:23:29 +0200

    Création du Type recette et ingédient, ajout du formulaire pour ajouter une recette (ne fonctionne toujours pas)

--- cousinetna/src/app/app.module.ts
+++ cousinetna/src/app/app.module.ts
@@ -7,6 +7,20 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AdddeletechangecomponentComponent } from './adddeletechangecomponent/adddeletechangecomponent.component';
 import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { DialogComponent } from './dialog/dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,11 +28,24 @@ import { HomeComponent } from './home/home.component';
     NavbarComponent,
     AdddeletechangecomponentComponent,
     HomeComponent,
+    FormComponent,
+    DialogComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatCardModule,
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
