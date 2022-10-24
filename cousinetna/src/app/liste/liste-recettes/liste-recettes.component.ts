5297f0cd52f3afe82a293868182f220720ecb5b0
Nada-ach <achahbounenada@gmail.com>
2022-10-24 20:59:29 +0200

    Liste de catégories de recettes

--- cousinetna/src/app/liste/liste-recettes/liste-recettes.component.ts
+++ cousinetna/src/app/liste/liste-recettes/liste-recettes.component.ts
@@ -55,7 +55,7 @@ export class ListeRecettesComponent implements OnInit {
           "photo": "https://cdn.vox-cdn.com/thumbor/dFTj61C1YkIBCN8IytARX65bSAA=/0x0:960x639/1270x953/filters:focal(404x244:556x396):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/63084252/foodlife.0.jpg",
           "nom": "Recette 1",
           "tempsHeures": 0,
-          "tempsMinutes": 6,
+          "tempsMinutes": 35,
           "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
           "idCategorie": "6353eeb25e2ab5aa25f05ef0",
           "idType": 0,
@@ -98,7 +98,7 @@ export class ListeRecettesComponent implements OnInit {
           ]
       },
       {
-          "photo": "https://cdn.vox-cdn.com/thumbor/dFTj61C1YkIBCN8IytARX65bSAA=/0x0:960x639/1270x953/filters:focal(404x244:556x396):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/63084252/foodlife.0.jpg",
+          "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRcWlsD9e2X7GGBz1yvrBAHxGoSZi1TZAc3g&usqp=CAU",
           "nom": "Recette 2",
           "tempsHeures": 1,
           "tempsMinutes": 52,
@@ -144,7 +144,7 @@ export class ListeRecettesComponent implements OnInit {
           ]
       },
       {
-          "photo": "https://cdn.vox-cdn.com/thumbor/dFTj61C1YkIBCN8IytARX65bSAA=/0x0:960x639/1270x953/filters:focal(404x244:556x396):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/63084252/foodlife.0.jpg",
+          "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKNnTCVhoUp0hsZrD4Wtlp7Kq2wCEcQAnkX3ULcOwwyioxJZSsO2c-wl0JCuCx1erfeJo&usqp=CAU",
           "nom": "Recette 3",
           "tempsHeures": 1,
           "tempsMinutes": 6,
@@ -190,7 +190,7 @@ export class ListeRecettesComponent implements OnInit {
           ]
       },
       {
-          "photo": "https://cdn.vox-cdn.com/thumbor/dFTj61C1YkIBCN8IytARX65bSAA=/0x0:960x639/1270x953/filters:focal(404x244:556x396):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/63084252/foodlife.0.jpg",
+          "photo": "https://www.eatthis.com/wp-content/uploads/sites/4/2018/12/healthy-french-toast-with-strawberries.jpg?resize=800,586&quality=82&strip=all",
           "nom": "Recette 4",
           "tempsHeures": 2,
           "tempsMinutes": 6,
@@ -236,7 +236,7 @@ export class ListeRecettesComponent implements OnInit {
           ]
       },
       {
-          "photo": "https://cdn.vox-cdn.com/thumbor/dFTj61C1YkIBCN8IytARX65bSAA=/0x0:960x639/1270x953/filters:focal(404x244:556x396):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/63084252/foodlife.0.jpg",
+          "photo": "https://www.eatthis.com/wp-content/uploads/sites/4/2019/01/healthy-scramble-eggs-salmon-asparagus-and-goat-cheese.jpg?resize=800,586&quality=82&strip=all",
           "nom": "Recette 5",
           "tempsHeures": 0,
           "tempsMinutes": 21,
@@ -282,7 +282,7 @@ export class ListeRecettesComponent implements OnInit {
           ]
       },
       {
-          "photo": "https://cdn.vox-cdn.com/thumbor/dFTj61C1YkIBCN8IytARX65bSAA=/0x0:960x639/1270x953/filters:focal(404x244:556x396):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/63084252/foodlife.0.jpg",
+          "photo": "https://www.eatthis.com/wp-content/uploads/sites/4/2019/11/breakfast-sweet-potato.jpg?resize=800,586&quality=82&strip=all",
           "nom": "Recette 6",
           "tempsHeures": 2,
           "tempsMinutes": 45,
@@ -328,7 +328,7 @@ export class ListeRecettesComponent implements OnInit {
           ]
       },
       {
-          "photo": "https://cdn.vox-cdn.com/thumbor/dFTj61C1YkIBCN8IytARX65bSAA=/0x0:960x639/1270x953/filters:focal(404x244:556x396):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/63084252/foodlife.0.jpg",
+          "photo": "https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/japanese-egg-pancakes-spinach-green-onions.jpg?resize=800,586&quality=82&strip=all",
           "nom": "Recette 7",
           "tempsHeures": 1,
           "tempsMinutes": 9,
--- cousinetna/src/app/liste/liste.module.ts
+++ cousinetna/src/app/liste/liste.module.ts
@@ -1,33 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListeRecettesComponent } from './liste-recettes.component';
+import { ListeComponent } from './liste.component';
 import { RouterModule, Routes } from '@angular/router';
-import {RatingModule} from 'primeng/rating';
+import { RatingModule } from 'primeng/rating';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {DividerModule} from 'primeng/divider';
 import { TableModule } from 'primeng/table';
-import { TabViewModule } from "primeng/tabview";
-import { DialogModule } from "primeng/dialog";
+import { TabViewModule } from 'primeng/tabview';
+import { DialogModule } from 'primeng/dialog';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
-import {SelectButtonModule} from 'primeng/selectbutton';
-import {SliderModule} from 'primeng/slider';
+import { SliderModule } from 'primeng/slider';
+import { SelectButtonModule } from 'primeng/selectbutton';
 import { ChipModule } from 'primeng/chip';
+import { ListeRecettesComponent } from './liste-recettes/liste-recettes.component';
+
 
 const routes: Routes = [
   {
     path: '',
+    component: ListeComponent
+  },
+  {
+    path: ':categorie',
     component: ListeRecettesComponent
   }
 ];
 
 @NgModule({
-  declarations: [ListeRecettesComponent],
+  declarations: [ListeComponent,ListeRecettesComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     RatingModule,
     FormsModule,    
-    DividerModule,
     TableModule,
     TabViewModule,
     DialogModule,
@@ -39,4 +43,4 @@ const routes: Routes = [
     
   ]
 })
-export class ListRecipesModule { }
+export class ListeModule { }
